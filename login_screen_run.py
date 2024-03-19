7af1a5539220857c74d50b9793354c3396e43c7f
Melik Baki Kul <melik_kul@outlook.com>
2024-03-19 22:12:42 +0300

    login screen convert
    Login Screen converted pyqt6 to py

---
+++ login_screen_run.py
@@ -0,0 +1,22 @@
+from PySide6.QtWidgets import *
+from ui_login_screen import Ui_MainWindow  
+from PySide6.QtCore import Qt
+from PySide6.QtGui import * 
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        #self.setWindowFlag(Qt.FramelessWindowHint)
+        
+        # Arayüzü oluşturmak için Ui_MainWindow sınıfından bir örnek oluşturun
+        self.ui = Ui_MainWindow()
+        # Arayüzü setupUi metoduyla ayarlayın
+        self.ui.setupUi(self)
+        
+        # Ana pencereyi göstermek için show metodunu çağırın
+        self.show()
+
+if __name__ == "__main__":
+    app = QApplication([])
+    window = MainWindow()
+    app.exec()
