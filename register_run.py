0a5494bbc23bfe82135890676513214a8f9aaf37
Melik Baki Kul <86568826+melikkul@users.noreply.github.com>
2024-04-02 21:25:35 +0300

    Register Screen

---
+++ register_run.py
@@ -0,0 +1,22 @@
+from PySide6.QtWidgets import *
+from ui_register import Ui_anasayfa
+from PySide6.QtCore import Qt
+from PySide6.QtGui import * 
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        #self.setWindowFlag(Qt.FramelessWindowHint)
+        
+        # Arayüzü oluşturmak için Ui_MainWindow sınıfından bir örnek oluşturun
+        self.ui = Ui_anasayfa()
+        # Arayüzü setupUi metoduyla ayarlayın
+        self.ui.setupUi(self)
+        
+        # Ana pencereyi göstermek için show metodunu çağırın
+        self.show()
+
+if __name__ == "__main__":
+    app = QApplication([])
+    window = MainWindow()
+    app.exec()
